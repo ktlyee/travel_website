02bfe021442fcaf841c35e09cc3853ef75ef0b4d
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-02-27 00:00:13 +0700

    [UPDATE] Card component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import CardItem from './CardItem'
 import './Cards.css'
+import img2  from '../assets/images/img-2.jpg'
+import img3  from '../assets/images/img-3.jpg'
+import img4  from '../assets/images/img-4.jpg'
+import img8  from '../assets/images/img-8.jpg'
+import img9  from '../assets/images/img-9.jpg'
 
 function Cards() {
     return (
@@ -10,9 +15,35 @@ function Cards() {
                 <div className="cards-wrapper">
                     <ul className="cards-items">
                         <CardItem
-                            src='../assets/images/img-9.jpg'
+                            src={img9}
                             text='Explore the hidden waterfall deep inside the Amazon Jungle'
                             label='Adventure'
+                            path='/sevices'
+                        />
+                        <CardItem
+                            src={img2}
+                            text='Travel through the Islands of Bali in a Private Cruise'
+                            label='Luxuly'
+                            path='/sevices'
+                        />
+                    </ul>
+                    <ul className="cards-items">
+                        <CardItem
+                            src={img3}
+                            text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+                            label='Mystery'
+                            path='/sevices'
+                        />
+                        <CardItem
+                            src={img4}
+                            text='Experience Football on Top of the Himilayan Mountains'
+                            label='Adventure'
+                            path='/sevices'
+                        />
+                        <CardItem
+                            src={img8}
+                            text='Ride through the Sahara Desert on a guided camel tour'
+                            label='Adenaline'
                             path='/sevices'
                         />
                     </ul>
