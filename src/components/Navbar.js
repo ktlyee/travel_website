261bfd87e46c69fa0b6786762b60d60b2b694bc3
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-02-27 11:18:55 +0700

    [UPDATE] Navbar
    change path services

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ function Navbar() {
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to='/srevices' className="nav-links" onClick={closeMobileMenu}>
+                            <Link to='/services' className="nav-links" onClick={closeMobileMenu}>
                                 Services
                             </Link>
                         </li>
